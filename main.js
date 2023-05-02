7adb6c4850a62fcaa7be3adf6ad6aea78dba6e40
JaimeBeltetonM <124618824+JaimeBeltetonM@users.noreply.github.com>
2023-05-02 13:01:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function trigger()
+{
+    document.getElementById("hover").addEventListener("click", popup);
+}
+
+function popup()
+{
+    alert("Haaland is going to win the CHAMPIONS LEAGUE!!!");
+}
